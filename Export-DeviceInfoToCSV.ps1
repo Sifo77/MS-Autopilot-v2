5a9552bec84efe259ab3ebb3d00d359d89677f82
Sifo77 <admin.sifo@gmail.com>
2024-11-18 13:02:13 +0100

    Add files via upload

---
+++ Export-DeviceInfoToCSV.ps1
@@ -0,0 +1,7 @@
+﻿$info = [PSCustomObject]@{
+    Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+    Model        = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+    SerialNumber = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
+}
+
+$info | Export-Csv -Path "C:\Temp\CorporateDevices.csv" -NoTypeInformation
---
+++ Get-DeviceInfo.ps1
@@ -0,0 +1,8 @@
+﻿$computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
+$bios = Get-CimInstance -ClassName Win32_BIOS
+
+[PSCustomObject]@{
+    Manufacturer = $computerSystem.Manufacturer
+    Model        = $computerSystem.Model
+    SerialNumber = $bios.SerialNumber
+}
